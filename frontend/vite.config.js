f590a9a3d4f3bbf144bfd3951e78b4ee68872a3d
Alberto Beguier <aabeguier@hotmail.com>
2024-09-19 11:32:03 -0300

    icono en index html deploy render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,15 @@ export default defineConfig({
         tailwindcss,
         autoprefixer,
       ],
+    },
+  },
+  build: {
+    outDir: 'dist',
+  },
+  publicDir: 'public',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
     },
   },
 })
